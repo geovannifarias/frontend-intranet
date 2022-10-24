48099c3bbd46fe906b4580ad8a46037b3076b2d7
geovannifarias <geovanni.farias.veliz@gmail.com>
2022-10-24 19:05:32 -0300

    cambio 1

--- back-end/src/controllers/index.controller.js
+++
@@ -1,6 +0,0 @@
-import {pool} from '../db.js'
-
-export const consultaEmpleado = async(req, res) => {
-    const [colaboradores] = await pool.query('SELECT*FROM colaborador')
-    res.json(colaboradores)
-}
--- back-end/src/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { consultaEmpleado } from "../controllers/index.controller.js";
-
-const router = Router()
-
-router.get('/ping', consultaEmpleado)
-
-export default router
